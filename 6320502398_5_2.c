d5aab70da0a0d9632cfc17617952072ecb5163f7
ntwww <nattawut.rod@ku.th>
2021-03-11 16:53:47 +0700

    2

--- 6320502398_5_2.c
+++ 6320502398_5_2.c
@@ -1,22 +1,26 @@
 #include<Stdio.h>
 int main()
 {
-    int n,i,j,x;
+    int n,i,j,q,x;
     scanf("%d",&n);
+    int n1=n;
     if(n%2==0)
     {
-        n-=1;
-        x=(n/2)-1;
+        n--;
+        q=1;
+        x=((n+q)/2)-1;
     }
     else
     {
+        q=0;
         x=n/2;
     }
-    for(i=0;i<n/2;i++)
+    printf("%d",x);
+    for(i=0;i<(n+q)/2;i++)
     {
         for(j=0;j<n;j++)
         {
-            if(j==(n/2)-1||j==(n/2)-1)
+            if(j==(n/2)-i||j==(n/2)+i)
             {
                 printf("*");
             }
@@ -25,4 +29,11 @@ int main()
         }
         printf("\n");
     }
+    /*for(i=0;<x+1;i++)
+    {
+        for(j=0;j<=;j++)
+        {
+
+        }
+    }*/
 }
