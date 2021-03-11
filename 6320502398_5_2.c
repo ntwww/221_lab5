088716561ac04809c7ff3d10964ad04a70128c76
ntwww <nattawut.rod@ku.th>
2021-03-11 17:50:59 +0700

    2

--- 6320502398_5_2.c
+++ 6320502398_5_2.c
@@ -1,21 +1,18 @@
-#include<Stdio.h>
+#include<stdio.h>
 int main()
 {
-    int n,i,j,q,x;
+    int n,i,j,x,q=0;
     scanf("%d",&n);
-    int n1=n;
-    if(n%2==0)
+    if(n%2==1)
     {
-        n--;
-        q=1;
-        x=((n+q)/2)-1;
+        x=((n+q)/2);
     }
     else
     {
-        q=0;
-        x=n/2;
+        n--;
+        q++;
+        x=(((n+q)/2)-1);
     }
-    printf("%d",x);
     for(i=0;i<(n+q)/2;i++)
     {
         for(j=0;j<n;j++)
@@ -25,21 +22,25 @@ int main()
                 printf("*");
             }
             else
+            {
                 printf("_");
+            }
         }
         printf("\n");
     }
-    for(i=0;i<x+1;i++)
+    for(i=0;i<=x;i++)
     {
-        for(j=0;j<=n;j++)
+        for(j=0;j<n;j++)
         {
-                if(j==(n/2)-i||j==(n/2)+i)
-            {
-                printf("*");
-            }
-                else
-                printf("_");
+            if(j==i || j==((n-1)-i))
+               {
+                   printf("*");
+               }
+               else
+                {
+                    printf("_");
+                }
         }
-        printf()
+        printf("\n");
     }
 }
